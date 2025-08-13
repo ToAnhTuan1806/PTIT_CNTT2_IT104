ba0f2cac795c3dd6aa31aa2a7487795407e2afa0
Tô Anh Tuấn <toanhtuan.18dz@gmail.com>
2025-08-13 19:33:31 +0700

    first commit

--- PTIT_CNTT2_IT104_Session07/PTIT_CNTT2_IT104_Session07_Bai9/Bai9.ts
+++ PTIT_CNTT2_IT104_Session07/PTIT_CNTT2_IT104_Session07_Bai9/Bai9.ts
@@ -1,108 +1,106 @@
-class Comment{
-    public id:number
-    public userId:number
-    public content:string
-    public replies: Comment[]=[]
+class Comment {
+    public id: number;
+    public userId: number;
+    public content: string;
+    public replies: Comment[] = [];
 
-    constructor(id:number, userId:number, content:string){
-        this.id=id
-        this.userId=userId
-        this.content=content
-    }
-    addReply(reply: Comment):void{
-        this.replies.push(reply)
-    }
-}
-
-class Post{
-    public id:number
-    public likes: number[]=[]
-    public comments: Comment[]=[]
-    public userId: number
-    public content: string
-
-    constructor(id:number, userId:number, content:string){
-        this.id=id
-        this.userId=userId
-        this.content=content
+    constructor(id: number, userId: number, content: string) {
+        this.id = id;
+        this.userId = userId;
+        this.content = content;
     }
 
-    addLike(userId:number):void{
-        if(!this.likes.includes(userId)){
-            this.likes.push(userId)
-        }
-    }
-
-    addComment(comment: Comment):void{
-        this.comments.push(comment)
+    addReply(reply: Comment): void {
+        this.replies.push(reply);
     }
 }
 
-class User{
-    public id:number
-    public posts:Post[]=[]
-    public followers:User[]=[]
-    constructor(id:number){
-        this.id=id
+class Post {
+    public id: number;
+    public userId: number;
+    public content: string;
+    public likes: number[] = [];
+    public comments: Comment[] = [];
+
+    constructor(id: number, userId: number, content: string) {
+        this.id = id;
+        this.userId = userId;
+        this.content = content;
     }
 
-    createPost(postId:number ,content:string):void{
-        let newPost= new Post(postId, this.id, content)
-        this.posts.push(newPost)
-    }
-
-    comment(postId:number, commentId:number, commentContent:string, allPosts:Post[]):void{
-        let post=  allPosts.find(p => p.id===postId)
-        if(post){
-            let newComment= new Comment(commentId, this.id, commentContent)
-            post.addComment(newComment)
+    addLike(userId: number): void {
+        if (!this.likes.includes(userId)) {
+            this.likes.push(userId);
         }
     }
 
-    follow(user:User):void{
-        if(!this.followers.includes(user)){
-            this.followers.push(user)
+    addComment(comment: Comment): void {
+        this.comments.push(comment);
+    }
+}
+
+class User {
+    public id: number;
+    public posts: Post[] = [];
+    public followers: User[] = [];
+
+    constructor(id: number) {
+        this.id = id;
+    }
+
+    createPost(postId: number, content: string): void {
+        const newPost = new Post(postId, this.id, content);
+        this.posts.push(newPost);
+    }
+
+    comment(post: Post, commentId: number, commentContent: string): void {
+        const newComment = new Comment(commentId, this.id, commentContent);
+        post.addComment(newComment);
+    }
+
+    follow(user: User): void {
+        if (!this.followers.includes(user)) {
+            this.followers.push(user);
         }
     }
 
-    likePost(postId:number, allPosts:Post[]):void{
-        let post= allPosts.find(p => p.id === postId)
-        if(post){
-            post.addLike(this.id)
-        }
+    likePost(post: Post): void {
+        post.addLike(this.id);
     }
 
-    viewFeed():Post[] {
-        let feed:Post[] = [];
-        for (let i=0; i<this.followers.length; i++) {
-            for (let j=0; j<this.followers[i].posts.length; j++) {
-                feed.push(this.followers[i].posts[j]);
+    viewFeed(): Post[] {
+        let feed: Post[] = [];
+        for (let user of this.followers) {
+            for (let post of user.posts) {
+                feed.push(post);
             }
         }
         return feed;
     }
 }
 
-let user1 = new User(1)
-let user2 = new User(2)
+const user1 = new User(1);
+const user2 = new User(2);
+const user3 = new User(3);
 
-user1.createPost(101, "Xin chào mọi người!")
-user1.createPost(102, "Hôm nay trời đẹp quá!")
+user2.createPost(101, "Bài đăng của user2");
+user3.createPost(201, "Bài đăng của user3");
 
-user2.follow(user1)
+user1.follow(user2);
+user1.follow(user3);
 
-let feed = user2.viewFeed()
-console.log("Feed của user2:", feed)
+const feed = user1.viewFeed();
+console.log("Feed của user1:");
+for (let post of feed) {
+    console.log(`PostID: ${post.id}, UserID: ${post.userId}, Content: ${post.content}`);
+}
 
-user2.likePost(101, user1.posts)
+if (user2.posts[0]) {
+    user1.likePost(user2.posts[0]);
+    console.log("Likes bài đăng của user2:", user2.posts[0].likes);
+}
 
-user2.comment(101, 201, "Chào bạn!", user1.posts)
-user2.comment(102, 202, "Thật tuyệt!", user1.posts)
-
-console.log("Bài đăng của user1:")
-for (let p of user1.posts) {
-    console.log(`Post ID: ${p.id}, Content: ${p.content}, Likes: ${p.likes.length}`)
-    for (let c of p.comments) {
-        console.log(`  Comment ID: ${c.id}, User: ${c.userId}, Content: ${c.content}`)
-    }
+if (user3.posts[0]) {
+    user1.comment(user3.posts[0], 301, "Bình luận của user1");
+    console.log("Comments bài đăng của user3:", user3.posts[0].comments);
 }
